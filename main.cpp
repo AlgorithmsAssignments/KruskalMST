399cbe875ec29f0badc50fe422ca8e36611fa7a4
Turupawn <ahmed.hn.43@gmail.com>
2017-03-02 17:49:51 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "Test.h"
+#include <iostream>
+#include "Edge.h"
+
+using namespace std;
+
+int** getKruskalMST(int** graph, int size, vector<Edge*> edges)
+{
+    return NULL;
+}
+
+int main ()
+{
+    test();
+    return 0;
+}
